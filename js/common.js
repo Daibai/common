91b0bdf7e5affa93849934d1acc334c7cb7b3deb
钟家建 <zhongjiajian@bytedance.com>
2019-01-12 16:48:17 +0800

    add function formateDate

---
+++ js/common.js
@@ -0,0 +1,35 @@
+/**
+ * js常用方法
+ */
+
+
+ /**
+  * 获取格式化的日期
+  * @param {objct} date - 日期对象 
+  * @param {object} option - 配置对象
+  * @param {boolean} option.hasTime - 是否需要时间(12:00:00) default：true
+  * @param {string} option.joinSymbol - 日期的连接符号 default："-"
+  * @return {string} - 返回格式化的日期【"2019-01-12 16:00:00"】
+  */
+
+   function formateDate(date,option={hasTime:true,joinSymbol:'-'}){
+    if(!date) date = new Date();
+    let _ = option.joinSymbol,
+        year = date.getFullYear(),
+        month = date.getMonth() + 1,
+        day = date.getDate();
+    if(month < 10) month = '0' + month;
+    if(day < 10) day = '0' + day;
+    if(option.hasTime){
+        let hours = date.getHours(),
+            minutes = date.getMinutes(),
+            seconds = date.getSeconds();
+        if(hours < 10) hours = '0' + hours;
+        if(minutes < 10) minutes = '0' + minutes;
+        if(seconds < 10) seconds = '0' + seconds;
+        return `${year}${_}${month}${_}${day} ${hours}:${minutes}:${seconds}`;
+    }else{
+        return `${year}${_}${month}${_}${day}`;
+    }
+}
+console.log(formateDate(null,{hasTime:false,joinSymbol:'/'}))
