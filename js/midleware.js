a1286c0d498fb3794673d0b7633f4a3705469c5a
钟家建 <zhongjiajian@bytedance.com>
2019-05-10 15:16:31 +0800

    优化中间件函数

--- js/midleware.js
+++ js/midleware.js
@@ -1,34 +1,53 @@
 class App {
     constructor(){
-        this.fnList = {};
-        this.num = 0;
+        this.slice = Array.prototype.slice;
+        this.fnList = [];
         this.store = {};
     }
-    use(){
-        this.num ++;
-        var oneList = [];
-        Array.prototype.slice.apply(arguments).map(fn => {
+    invertPromise(){
+        const fnList = [];
+        this.slice.call(arguments).map(fn => {
             if(typeof fn === 'function'){
-                oneList.push(fn);
-            }
-        })
-        this.fnList[this.num] = oneList;
-    }
-    async go(){
-        var fnList = [];
-        for(let i in this.fnList){
-            this.fnList[i].map(fn => {
-                fnList.push(function(store){
+                const p = (store)=>{
                     return new Promise(resolve => {
-                        fn(resolve);
-                    })
-                })
-            })
-        }
-        for(let i = 0; i < fnList.length; i++){
-            await fnList[i](this.store);
+                        fn(store,resolve);
+                    });
+                };
+                fnList.push(p);
+            }
+        });
+        return fnList;
+    }
+    register(arr){
+        this.fnList = this.fnList.concat(arr);
+    }
+    // use注册的callback，同步执行
+    use(...args){ 
+        this.register(this.invertPromise(...args));
+    }
+    // all注册的callback，异步执行，类似于Promise.all()
+    all(...args){
+        const all = this.invertPromise(...args);
+        this.fnList.push(all);
+    }
+    async go(...args){
+        this.register(this.invertPromise(...args));
+        try{
+            for(let i = 0; i < this.fnList.length; i++){
+                let fn = this.fnList[i];
+                if(Array.isArray(fn)){
+                    await Promise.all(fn.map(f=>f(this.store)));
+                }else{
+                    await fn(this.store);
+                }
+            }
+        }catch(err){
+            this.errorCallback(err);
         }
         
+    }
+    error(errorCallback){
+        if(typeof errorCallback === 'function') this.errorCallback = errorCallback;
     }
 };
 
